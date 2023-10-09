9196b895dba0c145c5c6ab3efebdeaa9f5563ac9
Asel <adelyakalenova@gmail.com>
2023-10-09 18:22:56 +0600

    first commit

--- js/home_works.js
+++ js/home_works.js
@@ -1,3 +1,4 @@
+//DZ1_1
 let gmailInput = document.querySelector("#gmail_input")
 let gmailButton = document.querySelector("#gmail_button")
 let gmailResult = document.querySelector("#gmail_result")
@@ -13,3 +14,26 @@ gmailButton.onclick = () => {
         gmailResult.style.color = 'red'
     }
 }
+
+//DZ1_02
+let counter = 0
+function moveBlock (){
+    const  child = document.querySelector('.child_block')
+    child.style.left = `${counter}px`
+    counter++
+    if (counter <= 450){
+        requestAnimationFrame(moveBlock)
+    }
+}
+moveBlock()
+
+
+
+
+
+
+
+
+
+
+
