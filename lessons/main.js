8083938f64a9029225dd4fcd88094cde462a8d5a
Asel <adelyakalenova@gmail.com>
2023-10-18 14:53:24 +0600

    dz3

---
+++ lessons/main.js
@@ -0,0 +1,55 @@
+// setTimeout(() => {
+//     console.log(1)
+// }, 2100)
+//
+// console.log(4)
+// setTimeout(() => {
+//     console.log(2)
+// }, 1000)
+//
+// setTimeout(() => {
+//     console.log(3)
+// }, 500)
+//
+// setTimeout(() => {
+//     console.log(5)
+// }, 50)
+// let num = 0
+//  const interval = setInterval(() => {
+//    num++
+//     console.log(num)
+// },1000)
+
+//EVENT LOOP
+
+
+// const doHomeWork = (finish,subject) => {
+//     alert(`starting my ${subject} home work!`)
+//     finish()
+// }
+//
+// const alertFinish = () => {
+//     return alert('FINISH!')
+// }
+// doHomeWork('subject,subject')
+
+
+
+
+// const calc = (a, d ) => console.log(a)
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// const newArr  = arr.map((indexElement, ) => {
+//     console.log(`Element: ${element}, index: ${indexElement}`)
+//     return element + indexElement
+// })
+//
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// arr.forEach((element, indexElement )=>{
+//     return console.log(element + indexElement)
+// })
+
+
+
